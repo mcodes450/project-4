1e07126b2b63658a7c4e05c696717d8431c758c2
Mohammed's School Account <159501876+mcodes450@users.noreply.github.com>
2026-05-24 16:46:21 -0400

    Update FindPaths.cc

--- FindPaths.cc
+++ FindPaths.cc
@@ -1,15 +1,45 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <fstream>
+#include <limits>
 
 #include "graph.h"
 
 using namespace std;
 
 int pathfindDriver(int argc, char **argv) {
-    
-    // Begin your code here. Feel free to add any helper functions or classes you need,
-    // as long as we only have to call this function to run the specified assignment.
+    if (argc != 3) {
+        return 0;
+    }
+    string graph_file = argv[1];
+    int start_vertex = stoi(argv[2]);
+
+    Graph graph;
+    if (!graph.loadGraphFromFile(graph_file)) {
+        return 0;
+    }
+
+    vector<double> distances;
+    vector<int> path_prefixes;
+
+    graph.dijkstra(start_vertex, distances, path_prefixes);
+
+    for (int i = 1; i <= graph.getNumVertices(); ++i) {
+        if (distances[i] == numeric_limits<double>::infinity()) {
+            cout << i << ": not_possible" << endl;
+        } else {
+            vector<int> path;
+            for (int curr = i; curr != -1; curr = path_prefixes[curr]) {
+                path.push_back(curr);
+            }
+            cout << i << ":";
+            for (auto it = path.rbegin(); it != path.rend(); ++it) {
+                cout << " " << *it;
+            }
+            cout << " cost: " << distances[i] << endl;
+        }
+    }
 
     return 0;
 }
